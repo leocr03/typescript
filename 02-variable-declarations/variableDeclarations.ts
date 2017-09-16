52b8945110121a6338cd9cf34f6d0748e9d136c3
Leonardo Cristofari <leocr03@gmail.com>
2017-09-16 14:04:51 -0300

    Added example to understand the var scope

--- 02-variable-declarations/variableDeclarations.ts
+++ 02-variable-declarations/variableDeclarations.ts
@@ -1,2 +1,21 @@
 import { Base } from "../base";
 
+function showVarScope() {
+    var a = 1;
+
+    function sumVar(){
+        a += 1;
+        return a;
+    }
+
+    console.log('a = ' + a);
+    console.log('sumVar() = ' + sumVar());
+    console.log('a = ' + a);
+    a = sumVar();
+    console.log('a = ' + a);
+    console.log('sumVar() = ' + sumVar());
+    console.log('a = ' + a);
+    console.log('sumVar() = ' + sumVar());
+}
+
+showVarScope();
